b44dad8e5d295f2a259f1f6d91af2dba1866219b
Florin <48189025+FlorinInfo@users.noreply.github.com>
2023-01-25 23:15:53 +0200

    Added Welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,16 @@ import MainLayout from "./Layouts/MainLayout";
 import DashboardLayout from "./Layouts/DashboardLayout";
 import Public from "./Views/Public";
 import Login from "./Views/Login";
+import Welcome from "./Views/Welcome";
 function App() {
   return (
       <Routes>
         <Route path="/" element={<MainLayout/>}>
             <Route index element={<Public/>}/>
             <Route path="login" element={<Login/>}/>
-            <Route path="dashboard" element={<DashboardLayout/>}></Route>
+            <Route path="dashboard" element={<DashboardLayout/>}>
+                <Route index element={<Welcome/>}></Route>
+            </Route>
         </Route>
       </Routes>
   );
