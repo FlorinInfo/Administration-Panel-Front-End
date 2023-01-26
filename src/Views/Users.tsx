30f133b0eeae0300438b63cf17cf0d50c951698f
Florin <48189025+FlorinInfo@users.noreply.github.com>
2023-01-26 22:48:15 +0200

    Added and configured axios

--- src/Views/Users.tsx
+++ src/Views/Users.tsx
@@ -1,6 +1,28 @@
-import {FunctionComponent}  from "react";
+import {FunctionComponent, useEffect, useState} from "react";
+import axiosConfig from "../api/defaultApi";
+
+interface UserI {
+    username: string;
+    roles:Array<string>;
+    active:boolean;
+}
 
 const Users:FunctionComponent = ()=> {
+    const [users, setUsers] = useState<UserI[]>([]);
+
+    const getUsers  = async ():Promise<void>=> {
+        const {data}:{data:Array<UserI>} = await axiosConfig.get("/users");
+        setUsers(data);
+    }
+
+    useEffect(()=>{
+        getUsers();
+    },[])
+
+    useEffect(()=>{
+        console.log(users);
+    },[users])
+    
     return (
         <>
         <h1>Users</h1>
